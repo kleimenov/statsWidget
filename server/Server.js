04924027c9f2374d005b0815a8b1f968fd2000c1
Иван <kleimenov416@gmail.com>
2021-02-18 09:56:01 -0500

    build out server and route return data.json

--- server/Server.js
+++ server/Server.js
@@ -6,24 +6,11 @@ const fs = require("fs");
 //please set PORT whatever you like, use CLI command: export PORT=(your chose)
 //in this case I will use PORT = 8080
 const PORT = process.env.PORT || 8080;
-
 const app = express();
 
-//app.use(express.static(path.join(__dirname, "data.json")));
-console.log(path.join(__dirname, "data.json"));
 const dataPath = path.join(__dirname, "data.json");
-console.log(dataPath);
-
-//this is route to wher I will keep data (JSON) and use this rout like URL to featch data for widget components
-/*
-app.get('/', (req, res) => {
-    res.send(data)
-})
-*/
-//const dataPath = './data.json'
 
 app.get("/", (req, res) => {
-  //fs.readFile('/Users/deelooc21/Desktop/Developer/Projects/assesment skills project/statswidget/server/data.json', 'utf8', (err, data) => {
   fs.readFile(dataPath, "utf8", (err, data) => {
     if (err) {
       throw err;
