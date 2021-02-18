1bee5746e419c62d37538e890d2462710df28b0d
Иван <kleimenov416@gmail.com>
2021-02-18 16:26:08 -0500

    create Areachart component

--- src/Main/Areachart.js
+++ src/Main/Areachart.js
@@ -3,7 +3,7 @@ import * as d3 from "d3";
 
 const Areachart = ({ dataElement }) => {
   console.log(dataElement);
-  return <div>Hello</div>;
+  return <div>{dataElement.date}</div>;
 };
 
 export default Areachart;
