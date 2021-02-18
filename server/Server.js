6cca2f7eea275b983df7662980581628c451a842
Иван <kleimenov416@gmail.com>
2021-02-18 10:36:07 -0500

    implement function getData, it is data parcer function, now client side and serve connected

--- server/Server.js
+++ server/Server.js
@@ -2,11 +2,16 @@ const express = require("express");
 //const data = require("./data.json");
 const path = require("path");
 const fs = require("fs");
+//This module helps React server get data without problems with headers. 
+//It isn't the most effective and safe way to share data between servers, 
+//but in this case, it will work well.
+const cors = require("cors");
 
 //please set PORT whatever you like, use CLI command: export PORT=(your chose)
 //in this case I will use PORT = 8080
 const PORT = process.env.PORT || 8080;
 const app = express();
+app.use(cors());
 
 const dataPath = path.join(__dirname, "data.json");
 
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import Filter from './Filter'
-import Chart from './Chart'
+import React, { useState, useEffect } from "react";
+import Filter from "./Filter";
+import Chart from "./Chart";
 
 const Main = () => {
+  const [data, setData] = useState([]);
 
-    return (
-        <div className="tmpMain">
-            <h3 className="tmpMainHeader">Main component</h3>
-            <Filter />
-            <Chart />
-        </div>
-    )
-}
+  useEffect(() => {
+    getData();
+  }, []);
 
-export default Main
+  const getData = async () => {
+    const response = await fetch("http://localhost:8080/");
+    const data = await response.json();
+    console.log(data)
+    setData(data);
+  };
+
+  console.log(data);
+
+  return (
+    <div className="tmpMain">
+      <h3 className="tmpMainHeader">Main component</h3>
+      <Filter />
+      <Chart />
+    </div>
+  );
+};
+
+export default Main;
