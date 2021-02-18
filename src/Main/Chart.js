1fa0d8c54a96a4e752d3a08594e9d90c10e18a80
Иван <kleimenov416@gmail.com>
2021-02-18 14:54:14 -0500

    complite first part of project

--- src/Main/Chart.js
+++ src/Main/Chart.js
@@ -3,7 +3,7 @@ import React from "react";
 const Chart = ({ data, currentCard }) => {
   //console.log('chart')
   //console.log(data[currentCard]);
-  //console.log(currentCard)
+  console.log(data)
 
   //data[currentCard]
 
@@ -12,7 +12,7 @@ const Chart = ({ data, currentCard }) => {
       <h5>{currentCard}</h5>
 
       {data[currentCard].map((item, index) => (
-        <p>{item.date}</p>
+        <p>{item.date} ~ {item.score}</p>
       ))}
     </div>
   );
