4379b9b4846bb5752739c237bdcd016c594900b8
Иван <kleimenov416@gmail.com>
2021-02-19 12:49:43 -0500

    finished with decorative elements, HTML markup and CSS selectors

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faGlobe, faMapPin } from "@fortawesome/free-solid-svg-icons";
 import Filter from "./Filter";
 import Chart from "./Chart";
 
@@ -28,13 +28,18 @@ const Main = () => {
       </div>
       <div className="bg-grey">
         <div className="container">
-          <FontAwesomeIcon icon={faGlobe} size="lg"/> 
+          <FontAwesomeIcon icon={faGlobe} size="lg" />
           <span className="icon-separator">Diagnostic tool</span>
+          <span className="icon-pin">
+            <FontAwesomeIcon icon={faMapPin} size="sm" />
+          </span>
         </div>
       </div>
-      <div className="main-graphs-holder">
-        <Filter data={gaugeData} setCard={setCard} />
-        <Chart data={areaData} currentCard={currentCard} />
+      <div className="container">
+        <div className="main-graphs-holder">
+          <Filter data={gaugeData} setCard={setCard} />
+          <Chart data={areaData} currentCard={currentCard} />
+        </div>
       </div>
     </div>
   );
