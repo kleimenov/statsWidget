8c048de61ae5f5ef8eff33ca29d61a667f26b21e
Иван <kleimenov416@gmail.com>
2021-02-19 08:00:55 -0500

    create Areachart component

--- src/Main/Areachart.js
+++ src/Main/Areachart.js
@@ -1,13 +1,13 @@
 import React from "react";
 import * as d3 from "d3";
-import { svg } from "d3";
+
 
 const Areachart = ({ dataElement }) => {
-  console.log(dataElement);
+  //console.log(dataElement);
 
   return (
    
-    <div>{dataElement.date}</div>
+    <div></div>
   )
     
 };
--- src/Main/Chart.js
+++ src/Main/Chart.js
@@ -1,23 +1,32 @@
 import React from "react";
-import Areachart from "./Areachart";
+import * as d3 from "d3";
+
+//import Areachart from "./Areachart";
 
 const Chart = ({ data, currentCard }) => {
   //console.log('chart')
-  //console.log(data[currentCard]);
+  console.log(data[currentCard]);
   //console.log(data)
-
   //data[currentCard]
+
+  const svg = d3.select(".graphHolder");
+  console.log(svg);
+
+  
 
   return (
     <div>
       <h5>{currentCard}</h5>
-
-      {data[currentCard].map((item, index) => (
-        //<p>{item.date} ~ {item.score}</p>
-        <Areachart key={index} dataElement={item} />
-      ))}
+      <div className="graphHolder"></div>
     </div>
   );
 };
 
 export default Chart;
+
+/*
+{data[currentCard].map((item, index) => (
+        //<p>{item.date} ~ {item.score}</p>
+        <Areachart key={index} dataElement={item} />
+      ))}
+*/
