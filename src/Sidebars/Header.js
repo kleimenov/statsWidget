a1b9d8d9b1996232a80d4ef427aa876d0be9f3e8
Иван <kleimenov416@gmail.com>
2021-02-19 11:24:47 -0500

    play with promesis, explore features and so on

--- src/Sidebars/Header.js
+++ src/Sidebars/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faFile, faOutdent } from '@fortawesome/free-solid-svg-icons'
+import { faFile, faOutdent, faFilter } from '@fortawesome/free-solid-svg-icons'
 
 //library.add(faCoffee)
 
@@ -14,7 +14,7 @@ const Header = () => {
         <h6 className="header-user-account">Logged in as General User</h6>
         <ul className="header-icons">
           <li>
-          <FontAwesomeIcon icon={faOutdent} color="white"/>
+          <FontAwesomeIcon icon={faFilter} color="white"/>
           </li>
           <li><FontAwesomeIcon icon={faOutdent} color="white"/></li>
           <li><FontAwesomeIcon icon={faOutdent} color="white"/></li>
