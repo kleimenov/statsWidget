a7d15f7abe345bba315b7a4cc840562dda007f14
Иван <kleimenov416@gmail.com>
2021-02-19 12:52:12 -0500

    finished with decorative elements, HTML markup and CSS selectors

--- src/Sidebars/Toolbar.js
+++ src/Sidebars/Toolbar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFile, faChartLine, faCubes, faFileExport, faMapPin } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFile,
+  faChartLine,
+  faCubes,
+  faFileExport,
+  faMapPin,
+} from "@fortawesome/free-solid-svg-icons";
 const Toolbar = () => {
   return (
     <div className="toolbar-container">
       <ul className="tools-list">
         <li>
-          <FontAwesomeIcon icon={faMapPin} color="white"/>
+          <FontAwesomeIcon icon={faMapPin} color="white" />
         </li>
         <li>
           <FontAwesomeIcon icon={faChartLine} color="white" />
