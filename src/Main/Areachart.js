78f37862287b95222491683989af785107ab1852
Иван <kleimenov416@gmail.com>
2021-02-20 11:33:05 -0500

    start working at area chart

--- src/Main/Areachart.js
+++ src/Main/Areachart.js
@@ -2,8 +2,8 @@ import React from "react";
 import { svg } from "d3";
 
 const Areachart = ({ data }) => {
-  //console.log(data);
-  data.map((item) => console.log(item.score))
+  console.log(data);
+  //data.map((item) => console.log(item.score))
 
 
   return (
