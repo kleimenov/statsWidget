0556663f936db00ce5183cedb35358fff2b780ad
Иван <kleimenov416@gmail.com>
2021-02-22 10:04:20 -0500

    version for heroku

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -14,12 +14,12 @@ const Main = () => {
   }, []);
 
   const getData = async () => {
-    const response = await fetch("http://localhost:3030/"); //NOTIFICATION If you will use different PORT on server-side, don't forget to change it here
+    const response = await fetch("https://stats-widget.herokuapp.com/"); // http://localhost:3030/ NOTIFICATION If you will use different PORT on server-side, don't forget to change it here
     const data = await response.json();
     setGaugeData(data.gaugeData);
     setAreaData(data.areaData);
   };
-
+  
   return (
     <div>
       <div className="container">
