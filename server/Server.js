5b5bb3285242e38df8141c182676024776816507
Иван <kleimenov416@gmail.com>
2021-02-22 09:11:39 -0500

    version for heroku

--- server/Server.js
+++ server/Server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3030;
 const app = express();
 app.use(cors());
 
