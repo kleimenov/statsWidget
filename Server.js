d74d0052d101cd2a9094cf6302a7a37024dfd0ff
Иван <kleimenov416@gmail.com>
2021-02-23 16:30:45 -0500

    maybe it will be successfull attempt to deploy it on heroku, who knows)

---
+++ Server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3030;
+const app = express();
+app.use(cors());
+
+const dataPath = path.join(__dirname, "data.json");
+
+app.get("/data", (req, res) => {
+  fs.readFile(dataPath, "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send(JSON.parse(data));
+  });
+});
+
+
+if (process.env.NODE_ENV === 'production') {
+  
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is listening ${PORT}............./`);
+});
---
+++ client/src/Main/Main.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe, faMapPin } from "@fortawesome/free-solid-svg-icons";
+import Filter from "./Filter";
+import Chart from "./Chart";
+
+const Main = () => {
+  const [currentCard, setCard] = useState("Quality Score");
+  const [gaugeData, setGaugeData] = useState([]);
+  const [areaData, setAreaData] = useState({ "Quality Score": [] });
+
+  useEffect(() => {
+    getData();
+  }, []);
+  
+  const getData = async () => {
+    const response = await fetch("/data"); 
+    const data = await response.json();
+    setGaugeData(data.gaugeData);
+    setAreaData(data.areaData);
+  };
+  
+  return (
+    <div>
+      <div className="container">
+        <h3 className="main-title">Performance management</h3>
+      </div>
+      <div className="bg-grey">
+        <div className="container">
+          <FontAwesomeIcon icon={faGlobe} size="lg" color="gray" />
+          <span className="icon-separator">Diagnostic tool</span>
+          <span className="icon-pin">
+            <FontAwesomeIcon icon={faMapPin} size="sm" color="gray" />
+          </span>
+        </div>
+      </div>
+      <div className="container">
+        <div className="main-graphs-holder">
+          <Filter data={gaugeData} setCard={setCard} />
+          <Chart data={areaData} currentCard={currentCard} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
